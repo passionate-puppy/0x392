044db41e78e4cc895c44b58498d34bbc2ddfeb53
passionate-puppy <116563668+passionate-puppy@users.noreply.github.com>
2022-10-25 02:53:59 +0800

    Refactor

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react'
 import styles from './Container.module.scss'
+import type { ReactNode } from 'react'
 
 export type ContainerProps = {
   children: ReactNode
--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,6 +1,6 @@
 import Container from 'components/common/Container'
-import { ReactNode } from 'react'
 import styles from './Layout.module.scss'
+import type { ReactNode } from 'react'
 
 export type LayoutProps = {
   children: ReactNode
