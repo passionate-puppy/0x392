86dfe874d5d12b4339e01f48ffc2ad42dcdcf802
passionate-puppy <116563668+passionate-puppy@users.noreply.github.com>
2022-10-25 23:52:21 +0800

    Update `<SocialLinks>`

--- src/components/common/SocialLinks/index.tsx
+++ src/components/common/SocialLinks/index.tsx
@@ -1,10 +1,17 @@
+import Button from 'components/base/Button'
 import styles from './SocialLinks.module.scss'
 
 function SocialLinks() {
   return (
     <div className={styles.container}>
-      <i className="fab fa-github" />
-      <i className="fab fa-linkedin" />
+      <Button
+        icon={<i className="fab fa-github" />}
+        iconOnly
+      />
+      <Button
+        icon={<i className="fab fa-linkedin" />}
+        iconOnly
+      />
     </div>
   )
 }
