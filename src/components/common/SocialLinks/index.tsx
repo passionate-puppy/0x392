0d4e4a3c9f47b3f1deaa3285f021615943982124
passionate-puppy <116563668+passionate-puppy@users.noreply.github.com>
2022-10-25 19:29:52 +0800

    Add `<SocialLinks>`

---
+++ src/components/common/SocialLinks/index.tsx
@@ -0,0 +1,12 @@
+import styles from './SocialLinks.module.scss'
+
+function SocialLinks() {
+  return (
+    <div className={styles.container}>
+      <i className="fab fa-github" />
+      <i className="fab fa-linkedin" />
+    </div>
+  )
+}
+
+export default SocialLinks
--- src/components/pages/IndexPage/index.tsx
+++ src/components/pages/IndexPage/index.tsx
@@ -1,12 +1,14 @@
 import Author from 'components/common/Author'
 import Header from 'components/common/Header'
 import Layout from 'components/common/Layout'
+import SocialLinks from 'components/common/SocialLinks'
 
 function IndexPage() {
   return (
     <Layout>
       <Header theme="primary" />
       <Author />
+      <SocialLinks />
     </Layout>
   )
 }
