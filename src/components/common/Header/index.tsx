84c02db59d6544cbf5a91c989d6964a490da4cf1
passionate-puppy <116563668+passionate-puppy@users.noreply.github.com>
2022-10-27 21:43:08 +0800

    Update `<PostPathPage>`

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,4 +1,6 @@
 import config from 'config'
+import routes from 'modules/routes'
+import Link from 'next/link'
 import styles from './Header.module.scss'
 
 export type HeaderProps = {
@@ -9,7 +11,11 @@ function Header({ theme }: HeaderProps) {
   return (
     <div className={styles.container}>
       {theme === 'primary' && <h1>{config.title}</h1>}
-      {theme === 'secondary' && <h2>{config.title}</h2>}
+      {theme === 'secondary' && (
+        <Link href={routes.home()}>
+          <h2>{config.title}</h2>
+        </Link>
+      )}
     </div>
   )
 }
--- src/components/pages/PostPathPage/index.tsx
+++ src/components/pages/PostPathPage/index.tsx
@@ -1,3 +1,6 @@
+import Author from 'components/common/Author'
+import Header from 'components/common/Header'
+import Layout from 'components/common/Layout'
 import PostContent from 'components/common/PostContent'
 import type { Post } from 'types/Post'
 
@@ -6,7 +9,13 @@ export type PostPathPageProps = {
 }
 
 function PostPathPage({ post }: PostPathPageProps) {
-  return <PostContent post={post} />
+  return (
+    <Layout>
+      <Header theme="secondary" />
+      <PostContent post={post} />
+      <Author />
+    </Layout>
+  )
 }
 
 export default PostPathPage
