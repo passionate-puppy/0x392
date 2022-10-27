3d7f4d624d22a9f6fb7b5b02c5f573337ad2967b
passionate-puppy <116563668+passionate-puppy@users.noreply.github.com>
2022-10-27 21:37:12 +0800

    Remove MDX-related packages

--- next.config.js
+++ next.config.js
@@ -4,15 +4,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
-module.exports = withMDX({
-  ...nextConfig,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+module.exports = nextConfig
